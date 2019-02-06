058f7980c48482f070c317dd54ee6a7a6d6d569e
VIET HOANG NGUYEN <viethoangnguyen2006@gmail.com>
2019-02-06 10:06:26 +0100

    add ContinentSingletonDatas

--- JoueurFootDATA/ClassData/ContinentSingletonData.cs
+++ JoueurFootDATA/ClassData/ContinentSingletonData.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace JoueurFootDATA
 {
-    #region MyRegion
+    #region ContinentSingletonData
     internal class ContinentSingletonData
     {
         public int Id { get; set; }
@@ -30,5 +30,28 @@ namespace JoueurFootDATA
         #endregion
     }
     #endregion
-
+    #region ContinentDatas : liste des continents
+    internal class ContinentSingletonDatas
+    {
+        public List<ContinentSingletonData> ListeContinent { get; set; }
+        FootContexte context = new FootContexte();
+        #region Constucteur par déffaut
+        ContinentSingletonDatas() { }
+        #endregion
+        #region Constucteur avec idContinent
+        public ContinentSingletonDatas(int idContinent)
+        {
+            ListeContinent = new List<ContinentSingletonData>();
+            var liste = context.GetContinent(idContinent);
+            foreach (var c in liste)
+            {
+                var continent = ContinentSingletonData.Instance;
+                continent.Id = c.Id;
+                continent.NomContinent = c.Nom;
+                ListeContinent.Add(continent);
+            }
+        }
+        #endregion
+    }
+    #endregion
 }
