d216fc7322f17eded284d1cd4e7845713b9da730
VIET HOANG NGUYEN <viethoangnguyen2006@gmail.com>
2019-02-06 08:50:16 +0100

    Add TestSingleton

---
+++ JoueurFootDATA/ClassData/TestSingleton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JoueurFootDATA
+{
+    internal sealed class TestSingleton
+    {
+        //private int Id { get; set; }
+        //private string Nom { get; set; }
+        static TestSingleton instance = null;
+        TestSingleton(){ }
+        public static TestSingleton Instance
+        {
+            get
+            {
+                if (instance is null)
+                {
+                    instance = new TestSingleton();
+                }
+                return instance;
+            }
+        }
+    }
+}
