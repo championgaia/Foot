6323d1377fdc2399a707fe613e7c0fcd2c546896
VIET HOANG NGUYEN <viethoangnguyen2006@gmail.com>
2019-02-06 09:29:19 +0100

    add ContinentSingletonData

---
+++ JoueurFootDATA/ClassData/ContinentSingletonData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JoueurFootDATA
+{
+    internal class ContinentSingletonData
+    {
+        public int Id { get; set; }
+        public string NomContinent { get; set; }
+        static ContinentSingletonData instance = null;
+        #region constructor par déffaut privé
+        ContinentSingletonData() { }
+        #endregion
+        #region gestion d'instanciation
+        public static ContinentSingletonData Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new ContinentSingletonData();
+                }
+                return instance;
+            }
+        }
+        #endregion
+    } 
+}
